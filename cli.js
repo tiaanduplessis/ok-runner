180bb2a7be141aa73272ebbcdcf66d4537c7a172
Tiaan <tiaanduplessis@hotmail.com>
2017-03-22 17:07:26 +0200

    Finish simple cli

--- cli.js
+++ cli.js
@@ -1,10 +1,24 @@
 #!/usr/bin/env node
 'use strict'
 
-const ok = require('./')
+const path = require('path')
+const fs = require('fs')
 
-const tasks = ok.args.unknown || []
+const chalk = require('chalk')
+const args = require('get-them-args')(process.argv.slice(2))
+const script = path.join(__dirname, (args.script || 'ok.js'))
+const tasks = args.unknown
 
-tasks.forEach((task) => {
-  ok.run(task)
-})
+if (fs.existsSync(script) && require.resolve(script)) {
+  const ok = require(script)
+
+  if (tasks.length) {
+    tasks.forEach((task) => {
+      ok.run(task)
+    })
+  } else {
+    ok.run('all')
+  }
+} else {
+  console.error(`🚨 ${chalk.red.bold('Error!')} The specified script does not exist or couldn't be resolved.`)
+}
--- example.js
+++
@@ -1,7 +0,0 @@
-const ok = require('./')
-
-ok
-  .task('Foo', () => console.log('Foo!'))
-  .task('lint Fix', 'standard --fix')
-  .task('Bar', 'echo Bar!')
-  .run('all')
