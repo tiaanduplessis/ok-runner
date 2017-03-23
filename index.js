a6fc5c5c3b564dcd20d64cdd241d555021a32374
Tiaan <tiaanduplessis@hotmail.com>
2017-03-23 20:38:06 +0200

    Switch from obj to stack for tasks

--- index.js
+++ index.js
@@ -7,29 +7,35 @@ const npmRunPath = require('npm-run-path')
 
 const exec = (cmd, options = {}, cb) => shell.exec(cmd, Object.assign({env: npmRunPath.env()}, options, cb))
 
+// TODO: - Switch to stack
+
 function ok () {
-  const _tasks = {}
+  const _tasks = []
   const _ok = {
     args,
     exec,
+    tasks: _tasks,
     task,
-    run
-  }
+    run}
 
   function task (name, handler) {
     if (typeof name !== 'string') {
       console.error(`🚨 ${chalk.red.bold('Error!')} Task(${name}) must have a name.`)
       return _ok
-      // throw new Error('The task name must be a string.')
     }
 
     if (typeof handler === 'string') {
-      _tasks[name] = () => {
-        console.log(`\n⌨ cmd - ${handler}`)
-        exec(handler)
-      }
+      _tasks.push({
+        name,
+        handler: () => {
+          console.log(`⌨ cmd  - ${chalk.green(handler)}`)
+          exec(handler)
+        }
+      })
     } else if (typeof handler === 'function') {
-      _tasks[name] = handler
+      _tasks.push({
+        name,
+        handler})
     } else {
       console.error(`🚨 ${chalk.red.bold('Error!')} Please provide a valid handler for task ${name}.`)
     }
@@ -44,26 +50,30 @@ function ok () {
     }
 
     if (name !== 'all' && (!_tasks[name] && !handler)) {
-      console.error(`🚨 ${chalk.red.bold('Error!')}Task ${chalk.bold(name)} doesn't exist. Please provide a handler or create the task.`)
+      console.error(`🚨 ${chalk.red.bold('Error!')}Task ${chalk.blue.bold(name)} doesn't exist. Please provide a handler or create the task.`)
       return _ok
     }
 
-    const timerName = `\n⚡ ${chalk.bold(name)} ran in`
+    // Create & start timer for running task
+    const timerName = `\n⚡ ${chalk.blue.bold(name)} ran in\n`
     console.time(timerName)
 
     if (name === 'all') {
-      console.log(`\n🎉 ${chalk.magenta('Running all tasks')}`)
-      for (let key in _tasks) {
-        console.log(`\n✨ Task - ${chalk.bold(key)}`)
-        _tasks[key](_ok)
-      }
+      console.log(`🎉 ${chalk.magenta('Running all tasks')}`)
+      _tasks.forEach((task) => {
+        console.log(`✨ Task - ${chalk.blue.bold(task.name)}`)
+        task.handler(_ok)
+      })
     } else {
-      if (!_tasks[name]) {
+      let currentTask = _tasks.filter((task) => task.name === name)
+
+      if (!currentTask.length) {
         _ok.task(name, handler)
       }
 
-      console.log(`\n✨ Task - ${chalk.bold(name)}`)
-      _tasks[name](_ok)
+      currentTask = _tasks.filter((task) => task.name === name)
+      console.log(`✨ Task - ${chalk.blue.bold(name)}`)
+      currentTask[0].handler(_ok)
     }
 
     console.timeEnd(timerName)
